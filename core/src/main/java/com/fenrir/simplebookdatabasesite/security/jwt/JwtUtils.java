c847e5947ac4865208a394399a1f690fde550bfa
Fenrir7734 <hitex1999@gmail.com>
2024-03-02 16:04:32 +0100

    Clean up

--- core/src/main/java/com/fenrir/simplebookdatabasesite/security/jwt/JwtUtils.java
+++ core/src/main/java/com/fenrir/simplebookdatabasesite/security/jwt/JwtUtils.java
@@ -4,12 +4,7 @@ import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
-import java.util.stream.Collectors;
 
 @Component
 public class JwtUtils {
@@ -17,23 +12,6 @@ public class JwtUtils {
 
     @Value("${fenrir.app.jwtSecret}")
     private String secret;
-
-    @Value("${fenrir.app.jwtExpiration}")
-    private int expiration;
-
-    public String generateJwtToken(UserDetails userDetails) {
-        String authorities = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
-
-        return Jwts.builder()
-                .setSubject(userDetails.getUsername())
-                .claim("roles", authorities)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime() + expiration))
-                .signWith(SignatureAlgorithm.HS512, secret)
-                .compact();
-    }
 
     public String extractUsername(String token) {
         return extractClaims(token).getSubject();
